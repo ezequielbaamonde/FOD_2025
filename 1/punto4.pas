15aa9efdf6f383441ceb46e24c2ae5f283590990
ezequielbaamonde <ezequielitobaamonde@gmail.com>
2025-03-20 12:02:37 -0300

    Avances 20/03/2025

--- 1/punto4.pas
+++ 1/punto4.pas
@@ -83,7 +83,8 @@ procedure impEmp(var data: archivo);
 procedure impEdad(var data: archivo);
  var emp: empleados; existe: boolean;
  begin
-	reset(data);
+	existe:= false; //Inicialmente
+	reset(data); //Apertura
 	writeln('');
 	while not eof(data) do begin
 		read(data, emp);
@@ -96,7 +97,7 @@ procedure impEdad(var data: archivo);
 			writeln('');
 		end;
 	end;
-	close(data);	
+	close(data); //Cierre de archivo
 	if (existe = true) then writeln('Todos los empleados mayores a 70 han sido listados.')
 	else writeln('No hay empleados mayores de 70 anios.');
  end;
@@ -114,30 +115,100 @@ procedure listado(var data: archivo);
 	impEdad(data);
 	
  end;
- 
-var registros: archivo; op: char; nomFis: String[20];
+
+procedure add(var data: archivo);
+ var empData, empLec: empleados; esta: boolean; fin: integer;
+ begin
+	fin:= 1; //Inicialmente es 1
+	while (fin = 1) do begin
+		esta:= false; //inicialmente falso
+		lecturaEmpleado(empLec);
+		reset(data); //apertura archivo
+		while not eof(data) do begin
+			read(data, empData);
+			if (empLec.num = empData.num)then begin
+				esta:= true;
+			end;
+		end;
+		if (esta = true) then writeln('Ya existe un empleado con ese numero de EMPLEADO.')
+		else begin 
+			write(data, empLec);
+			writeln('Empleado aniadido con exito!')
+		end;
+		writeln('');
+		writeln('Quiere aniadir otro empleado? (1 SI / 0 NO): ');
+		readln(fin);
+	end;
+	close(data); //Cierre
+ end;
+
+procedure update(var data: archivo);
+ var numEmpleado: integer; empData: empleados; newAge: integer; esta: boolean;
+ begin
+	esta:= false;
+	writeln('Ingrese el numero de empleado a actualizar');
+	readln(numEmpleado);
+	reset(data); //Apertura
+	while not eof (data) do begin
+		read(data, empData);
+		if (empData.num = numEmpleado) then begin
+			writeln('Ingrese la nueva edad:');
+			readln(newAge);
+			empData.edad:= newAge; //Colocamos nueva edad
+			seek(data, FilePos(data)-1);
+			write (data, empData);
+			esta:= true;
+		end;
+	end;
+	if (esta=true) then writeln('Empleado ', numEmpleado, ' actualizado con exito.')
+	else writeln('No existe un empleado con ese numero de empleado.');
+	close(data); //Cierre
+ end;
+
+procedure exp(var data: archivo; var dataT: Text);
+ var nomText: String; emp: empleados;
+ begin
+	writeln('Ingrese un nombre para el archivo de texto: ');
+	readln(nomText);
+	assign(dataT, nomText); //NomLógico, NomFísico
+	reset(data); //Abro archivo binario
+	rewrite(dataT); //Creo y abro archivo de texto 
+	while not eof(data) do begin
+		read(data, emp); //Leo empleado del archivo binario
+		with emp do writeln(num:5, ape:5, nom:5, edad:5, DNI:5); //Escribo en pantalla el registro
+		with emp do writeln(dataT, ' ', num, ' ', ape, ' ', nom, ' ', edad, ' ', DNI, ' '); {Escribe
+		en el archivo de texto los campos separados por un caracter en blanco}
+		
+	end;
+	close(data); close (dataT);
+ end;
+
+
+var registros: archivo; op: char; nomFis: String[20]; texto: Text;
 BEGIN
     writeln('Ingrese un nombre para el archivo a crear o utilizar: ');
 	readln(nomFis);
 	assign(registros, nomFis); //asignamos valor logico con valor fisico
+	
 	writeln('**MENU DE OPERACIONES**');
 	writeln(' A: Creacion y Carga de empleados.');
 	writeln(' B: Listar archivo.');
+	writeln(' C: Aniadir un empleado.');
+	writeln(' D: Actualizar empleado.');
+	writeln(' E: Exportar a archivo de TEXTO.');
+	
 	writeln('Ingrese una opcion del menu: ');
 	readln (op);
-	{while (op <> 'Z') do begin //para salir del programa}
+	{while (op <> 'Z') do begin //bucle para mantener el programa en ejec}
 	case op of
 		'A': creacion(registros);
 		'B': listado(registros);
+		'C': add(registros);
+		'D': update(registros);
+		'E': exp(registros, texto);
 	else
 		writeln('La opcion es incorrecta.');
 	end;
-		{writeln('**MENU DE OPERACIONES**');
-		writeln(' A: Creacion y Carga de empleados.');
-		writeln(' B: Listar archivo.');
-		writeln('Ingrese una opcion del menu: ');
-		readln (op);
-	end;}
 	writeln('********************************');
 	writeln('Todas las operaciones han sido realizadas.');
 END.
